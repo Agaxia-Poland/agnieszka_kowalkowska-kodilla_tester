a2e859bd85fb3a5854ff91d19b813635de86ac6e
Agnieszka Kowalkowska <agnieszkakowalkowska@gmail.com>
2020-06-17 16:43:11 +0200

    User

--- kodilla-intro/src/main/java/User.java
+++ kodilla-intro/src/main/java/User.java
@@ -1,40 +1,50 @@
 public class User {
     String user; //2 atrybuty: imię i wiek
-    int age;
+    String age;
+
+    public User(String peter, int i) {
+
+    }
 
     private static String user() { //dwie metody (gettery), które zwrócą (return) wartość pól z imieniem i wiekiem
-        String user = "Andrew"; "Peter"; "Mary"; "Ann";
+        String user = "Andrew, Peter, Mary, Ann";
 
         return user;
     }
 
-    private static int age() {
-        int age = 12, 22, 32, 42;
+    private static String age() {
+        String age = "12, 22, 32, 42";
 
         return age;
+
     }
-}
 
 
-    public static void main(String[] args) {
+    public class Users {
+        String users;
 
-        private String[] users; //utworzenie tablicy
-        private int size;
-
-        public User() { //konstruktor wyświetlający obiekty
+        public Users(String users) { //konstruktor wyświetlający obiekty
+            this.users = users;
+            
             User andrew = new User("Andrew", 12);
             User peter = new User("Peter", 22);
             User mary = new User("Mary", 32);
             User ann = new User("Ann", 42);
 
-            User[] users = {andrew, peter, mary, ann}; //utworzona tablica
+            User[] users = new String[] {"Andrew", "Peter", "Mary", "Ann"}; //utworzona tablica
             for (int i = 0; i < users.length; i++) {  //wyświetlenie wszystkich elementów tablicy
                 System.out.println(users[i]);
             }
             int age = 0; //suma i średnia wieku użytkowników
             for (int i = 0; i < users.length; i++) {
-                age = age + users[i];
+                age = user + age[i];
             }
         }
     }
+
+    public static void main(String[] args) {
+        boolean user;
+        System.out.println(user);
+    }
+
 }
