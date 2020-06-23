a6cf647e9c0b35cf39e1b53ea74a7e746c84231d
Agnieszka Kowalkowska <agnieszkakowalkowska@gmail.com>
2020-06-23 21:16:17 +0200

    InstrukcjaWarunkowaSwitch

--- kodilla-intro/src/main/java/AdvCalcApplication.java
+++ kodilla-intro/src/main/java/AdvCalcApplication.java
@@ -1,8 +1,8 @@
-//public class AdvCalcApplication {
-//    public static void main(String[] args) {
-//        AdvCalculator calculator = new AdvCalculator();
-//        double result = calculator.calculate();
-//
-//        System.out.println("Result: " + result);
-//    }
-//}
+public class AdvCalcApplication {
+    public static void main(String[] args) {
+        AdvCalculator calculator = new AdvCalculator();
+        double result = calculator.calculate();
+
+        System.out.println("Result: " + result);
+    }
+}
