28cd584767b674de6c3ba11431e2b5d8c645f084
Agnieszka Kowalkowska <agnieszkakowalkowska@gmail.com>
2020-06-27 11:22:42 +0200

    Application

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -1,14 +1,14 @@
-public class RandomNumbers {
-    public int getCountOfRandomNumber(int max) {
-
-        Random random = new Random();
-        int result = 0;
-        int sum = 0;
-        while (sum < max = 5000) {
-            int temp = random.nextInt(30);
-            sum = sum + temp;
-            result++;
-        }
-        return result;
-    }
-}
+//public class RandomNumbers {
+//    public int getCountOfRandomNumber(int max) {
+//
+//        Random random = new Random();
+//        int result = 0;
+//        int sum = 0;
+//        while (sum < max = 5000) {
+//            int temp = random.nextInt(30);
+//            sum = sum + temp;
+//            result++;
+//        }
+//        return result;
+//    }
+//}
