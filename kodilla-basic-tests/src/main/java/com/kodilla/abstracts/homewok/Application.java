527ec942baafb1b03a813d26cd85945e3de28ebc
Agnieszka Kowalkowska <agnieszkakowalkowska@gmail.com>
2020-08-21 20:41:57 +0200

    task-3.2.KlasyAbstrakcyjne

--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homewok/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homewok/Application.java
@@ -3,20 +3,16 @@ package com.kodilla.abstracts.homewok;
 public class Application {
 
     public static void main(String[] args) {
-        Shape square = new Square();
-        Square object = new Square();
-        object.giveSurfaceArea();
-        object.giveCircumference();
+        Shape square = new Square(2.5);
+        System.out.println("Square with side 2.5 has area " + square.getSurface());
+        System.out.println("Square with side 2.5 has circumference " + square.getCircumference());
 
-        Shape rectangle = new Rectangle();
-        Rectangle object2 = new Rectangle();
-        object2.giveSurfaceArea();
-        object2.giveCircumference();
+        Shape triangle = new Triangle(6, 4, 5,5);
+        System.out.println("Triangle with base 6, side 1 4, side 2 5, height 5 has area " + triangle.getSurface());
+        System.out.println("Triangle with base 6, side 1 4, side 2 5, height 5 has circumference " + triangle.getCircumference());
 
-        Shape square2 = new Square2();
-        Square2 object3 = new Square2();
-        object3.giveSurfaceArea();
-        object3.giveCircumference();
-
+        Shape circle = new Circle(3);
+        System.out.println("Circle with radius 3 has area " + circle.getSurface());
+        System.out.println("Circle with radius 3 has circumference " + circle.getCircumference());
     }
 }
