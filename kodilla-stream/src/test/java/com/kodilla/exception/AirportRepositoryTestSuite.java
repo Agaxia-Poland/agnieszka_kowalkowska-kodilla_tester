465a5cad3cbaa04792a970562637560cb510ce74
Agnieszka Kowalkowska <agnieszkakowalkowska@gmail.com>
2020-09-08 11:31:04 +0200

    task-6.4

---
+++ kodilla-stream/src/test/java/com/kodilla/exception/AirportRepositoryTestSuite.java
@@ -0,0 +1,28 @@
+package com.kodilla.exception;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class AirportRepositoryTestSuite extends TestCase {
+
+    @Test
+    public void testIsAirportInUse() throws AirportNotFoundException {
+        //given
+        AirportRepository airportRepository = new AirportRepository();
+        //when
+        boolean isWarsawInUse = airportRepository.isAirportInUse("Warsaw");
+        //then
+        assertTrue(isWarsawInUse);
+    }
+
+    @Test(expected = AirportNotFoundException.class)
+    public void testIsAirportInUse_withException() throws AirportNotFoundException {
+        //given
+        AirportRepository airportRepository = new AirportRepository();
+        //when
+        boolean isWarsawInUse = airportRepository.isAirportInUse("Vienna");
+        //then
+        assertTrue(isWarsawInUse);
+    }
+
+}
