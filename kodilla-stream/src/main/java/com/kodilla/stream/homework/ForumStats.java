520443b9c632330eddda4d535758027e0292b10b
Agnieszka Kowalkowska <agnieszkakowalkowska@gmail.com>
2020-09-08 09:41:26 +0200

    task-6.3

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -0,0 +1,15 @@
+package com.kodilla.stream.homework;
+
+import com.kodilla.stream.UsersRepository;
+
+public class ForumStats {
+    public static void main(String[] args) {
+        double avg = UsersRepository.getUsersList()
+                .stream()
+                .map(u -> u.getAge())
+                .mapToInt(n -> n)
+                .average()
+                .getAsDouble();
+        System.out.println(avg);
+    }
+}
