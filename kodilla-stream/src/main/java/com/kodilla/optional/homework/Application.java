329605d3bfdd5f0b6f3c4d734839bdbcf43b0f47
Agnieszka Kowalkowska <agnieszkakowalkowska@gmail.com>
2020-09-09 18:26:25 +0200

    task-6.2.1

--- kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
+++ kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
@@ -1,15 +1,23 @@
 package com.kodilla.optional.homework;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class Application {
     public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
         Student student = new Student("Student1", new Teacher("Teacher1"));
-        Student student2 = new Student("Student2", new Teacher("Teacher2"));
+        Student student1 = new Student("Student2", new Teacher("Teacher2"));
+        Student student3 = new Student("Student3", new Teacher(null));
+        Student student4 = new Student("Student4", new Teacher(null));
+        Student student5 = new Student("Student5", new Teacher("Teacher1"));
 
 
         Optional<Student> optionalStudent = Optional.ofNullable(student);
 
         optionalStudent.ifPresent(u -> System.out.println(u.getName()));
+
+//        for each
     }
 }
