1037e352c6935f80298042eaad212311957fc853
Agnieszka Kowalkowska <agnieszkakowalkowska@gmail.com>
2020-09-09 17:49:50 +0200

    task-7.1

--- kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/homework/UserValidator.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/homework/UserValidator.java
@@ -1,9 +1,34 @@
 package com.kodilla.parametrized_tests.homework;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserValidator {
+    public static void main(String[] args) {
+        List<String> usernames = new ArrayList<String>();
+        // valid usernames
+        usernames.add("alice89");
+        usernames.add("aliceZOR_123");
+        usernames.add("ALICE_ZOR-$4");
+        //invalid usernames
+        usernames.add("# .,");
+        usernames.add("----0o-@");
+        usernames.add("^ A A a a _ _*");
+
+        List<String> emails = new ArrayList<String>();
+        // valid email addresses
+        emails.add("alice@example.com");
+        emails.add("alice.bob@example.co.in");
+        emails.add("alice#@example.me.org");
+        //invalid email addresses
+        emails.add("alice.example.com");
+        emails.add("alice#example.com");
+        emails.add("@example.me.org");
+
+    }
+
     public boolean validateUsername(String username) {
         return username.matches("^[a-zA-Z0-9._-]{3,}$");
     }
@@ -17,4 +42,5 @@ public class UserValidator {
         }
         return false;
     }
+
 }
--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/UserValidatorTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/UserValidatorTestSuite.java
@@ -1,25 +1,21 @@
 package com.kodilla.parametrized_tests.homework;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UserValidatorTestSuite {
-    private UserValidator validator = new UserValidator();
-
-//    @ParameterizedTest
-//    @ValueSource(strings = {"^[a-zA-Z0-9._-]{3,}$"})
-//    public void shouldReturnTrueIfUsernameHasAboveStrings(String text) {
-//        assertTrue(validator.validateUsername(text));
-//    }
+    private final UserValidator validator = new UserValidator();
 
     @ParameterizedTest
-    @ValueSource(strings = {"^[a-zA-Z0-9._-]{3,}$"})
-    public void shouldReturnFalseIfUsernameHasNotAboveStrings(String text) {
-        assertFalse(validator.validateUsername(text));
+    @CsvSource(value = "^[a-zA-Z0-9._-]{3,}$")
+    public void shouldValidateUsername(String input, String expected) {
+        assertEquals(expected, validator.validateUsername(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = "^([_a-zA-Z0-9-]+(\\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*(\\.[a-zA-Z]{1,6}))?$")
+    public void shouldValidateEmail(String input, String expected) {
+        assertEquals(expected, validator.validateEmail(input));
     }
 }
-
-
