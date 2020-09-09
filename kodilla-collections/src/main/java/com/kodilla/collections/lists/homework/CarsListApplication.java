a5ddc2d4b517ca32ab475e7dcbab47938ea5e46d
Agnieszka Kowalkowska <agnieszkakowalkowska@gmail.com>
2020-09-09 13:52:07 +0200

    task-4.3.1

--- kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarsListApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarsListApplication.java
@@ -1,56 +1,34 @@
-package com.kodilla.collections.lists.homework;
-
-import com.kodilla.collections.interfaces.homework.Car;
-import com.kodilla.collections.interfaces.homework.Ford;
-import com.kodilla.collections.interfaces.homework.Opel;
-import com.kodilla.collections.interfaces.homework.Toyota;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class CarsListApplication {
     public static void main(String[] args) {
-        List<Ford> cars = new ArrayList<>();
-        cars.add(new Ford());
-        cars.add(new Ford());
-        cars.add(new Ford());
-
-        List<Opel> cars1 = new ArrayList<>();
-        cars1.add(new Opel());
-        cars1.add(new Opel());
-        cars1.add(new Opel());
-
-        List<Toyota> cars2 = new ArrayList<>();
-        cars2.add(new Toyota());
-        cars2.add(new Toyota());
-        cars2.add(new Toyota());
-
-        cars.remove(1);
-        cars1.remove(cars1);
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Ford("f1", 22, 3));
+        cars.add(new Ford("f2", 1, 45));
+        cars.add(new Ford("f21", 1, 415));
+        cars.add(new Ford("f5", 15, 425));
+        cars.add(new Ford("f21234", 1231, 451));
+        cars.add(new Opel("o1", 22, 3));
+        cars.add(new Opel("o2", 23, 4));
+        cars.add(new Opel("o3", 34, 235));
+        cars.add(new Toyota("t1", 78, 89));
+        cars.add(new Toyota("t2", 320, 48));
+        cars.add(new Toyota("t3", 240, 60));
 
         System.out.println(cars.size());
-        System.out.println(cars1.size());
-        System.out.println(cars2.size());
+
+        cars.remove(1);
+        System.out.println(cars.size());
+
+        Ford carToRemove = new Ford("f5", 15, 425);
+        cars.remove(carToRemove);
+        System.out.println(cars.size());
 
         for (Car car : cars) {
-            if (car.getSpeed() > 200)
-                System.out.println("----------------------------");
-                System.out.println(car + ", name: " + car.getName());
-                System.out.println(car + ", speed: " + car.getSpeed());
-        }
-
-        for (Car car : cars1) {
-            if (car.getSpeed() > 200)
-                System.out.println("----------------------------");
-                System.out.println(car + ", name: " + car.getName());
-                System.out.println(car + ", speed: " + car.getSpeed());
-        }
-
-        for (Car car : cars2) {
-            if (car.getSpeed() > 200)
-                System.out.println("----------------------------");
-                System.out.println(car + ", name: " + car.getName());
-                System.out.println(car + ", speed: " + car.getSpeed());
+            if (car.getSpeed() > 200) {
+                car.describeCar();
+            }
         }
     }
 }
