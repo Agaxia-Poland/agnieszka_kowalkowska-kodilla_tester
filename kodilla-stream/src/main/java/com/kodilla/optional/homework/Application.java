f107adfc66627c632bfa5766141940c2718c95c1
Agnieszka Kowalkowska <agnieszkakowalkowska@gmail.com>
2020-09-11 17:53:28 +0200

    task-6.2.1

--- kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
+++ kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
@@ -3,30 +3,35 @@ package com.kodilla.optional.homework;
 import java.util.*;
 
 public class Application {
+
     public static void main(String[] args) {
-        Map<Student, Teacher> students = new HashMap<>();
-        Student piotr = new Student("Piotr Nowak", "Anna Gadomska");
-        Student adam = new Student("Adam Nowak", "Mateusz Kowalski");
-        Student ewa = new Student("Ewa Kowalska", "Anna Gadomska");
 
-        Teacher piotrTeacher = new Teacher("Anna Gadomska");
-        Teacher adamTeacher = new Teacher("Mateusz Kowalski");
-        Teacher ewaTeacher = new Teacher("Anna Gadomska");
+        Map<Teacher, Student> students = new HashMap<>();
+        Teacher teacher = new Teacher("name");
+        Teacher t1 = new Teacher("Anna Gadomska");
+        Teacher t2 = new Teacher("Adam Kowalski");
+        Teacher t3 = new Teacher("Rafał Kalinowski");
 
-        students.put(piotr, piotrTeacher);
-        students.put(adam, adamTeacher);
-        students.put(ewa, ewaTeacher);
+        Student s1 = new Student("Piotr Nowak", t1);
+        Student s2 = new Student("Stasiek Nowacki", t2);
+        Student s3 = new Student("Adrian Jankowski", null);
 
-        System.out.println(students.get(piotr));
-        System.out.println(students.get(adam));
-        System.out.println(students.get(ewa));
+        students.put(t1, s1);
+        students.put(t2, s2);
+        students.put(null, s3);
 
-//        for (Map.Entry<Student, Teacher> studentEntry : students.entrySet()
-//        System.out.println(studentEntry.getKey().getName() + " " + studentEntry.getValue().getTeacher);
-//        )
-//
-//        Optional<Student> optionalStudent = Optional.ofNullable(student);
-//
-//        optionalStudent.ifPresent(u -> System.out.println(u.getName()));
+        System.out.println(students.get(s1));
+        System.out.println(students.get(s2));
+        System.out.println(students.get(s3));
+
+
+        Optional<Teacher> optionalTeacher = Optional.ofNullable(teacher);
+
+        optionalTeacher.ifPresent(u -> System.out.println(u.getName()));
+
+
+        for (Map.Entry<Teacher, Student> teacherEntry : students.entrySet()) {
+            System.out.println(teacherEntry.getKey().getName() + " " + teacherEntry.getValue().getName());
+        }
     }
 }
