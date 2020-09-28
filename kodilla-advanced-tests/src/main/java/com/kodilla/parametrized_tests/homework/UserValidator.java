15f753f6d8f9ef10907c4b3a9db4cb44691356bf
Agnieszka Kowalkowska <agnieszkakowalkowska@gmail.com>
2020-09-28 21:07:39 +0200

    task-7.1

--- kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/homework/UserValidator.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/homework/UserValidator.java
@@ -1,33 +1,9 @@
 package com.kodilla.parametrized_tests.homework;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserValidator {
-//    public static void main(String[] args) {
-//        List<String> usernames = new ArrayList<String>();
-//        // valid usernames
-//        usernames.add("alice89");
-//        usernames.add("aliceZOR_123");
-//        usernames.add("ALICE_ZOR-$4");
-//        //invalid usernames
-//        usernames.add("# .,");
-//        usernames.add("----0o-@");
-//        usernames.add("^ A A a a _ _*");
-//
-//        List<String> emails = new ArrayList<String>();
-//        // valid email addresses
-//        emails.add("alice@example.com");
-//        emails.add("alice.bob@example.co.in");
-//        emails.add("alice#@example.me.org");
-//        //invalid email addresses
-//        emails.add("alice.example.com");
-//        emails.add("alice#example.com");
-//        emails.add("@example.me.org");
-//
-//    }
 
     public boolean validateUsername(String username) {
         return username.matches("^[a-zA-Z0-9._-]{3,}$");
