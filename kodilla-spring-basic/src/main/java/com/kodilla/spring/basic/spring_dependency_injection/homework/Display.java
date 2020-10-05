a7bc77efc1b601af7dec565522efa8759d6c2b37
Agnieszka Kowalkowska <agnieszkakowalkowska@gmail.com>
2020-10-05 14:50:16 +0200

    task-9.2

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/Display.java
@@ -0,0 +1,9 @@
+package com.kodilla.spring.basic.spring_dependency_injection.homework;
+
+public class Display {
+    double value;
+
+    private void display(double value) {
+        System.out.println(value);
+    }
+}
---
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_dependency_injection/homework/CalculatorTestSuite.java
@@ -0,0 +1,41 @@
+package com.kodilla.spring.basic.spring_dependency_injection.homework;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CalculatorTestSuite {
+//
+//    @Test
+//    public void shouldReturnCorrectSum() {
+//        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
+//        Calculator bean = context.getBean(Calculator.class);
+//        double value = bean.add(10, 2);
+//        Assertions.assertNotNull(value);
+//        }
+//
+//    @Test
+//    public void shouldReturnCorrectSubtraction() {
+//        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
+//        Calculator bean = context.getBean(Calculator.class);
+//        double value = bean.subtract(10, 2);
+//        Assertions.assertNotNull(value);
+//    }
+//
+//    @Test
+//    public void shouldReturnCorrectMultiplication() {
+//        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
+//        Calculator bean = context.getBean(Calculator.class);
+//        double value = bean.multiply(10, 2);
+//        Assertions.assertNotNull(value);
+//    }
+//
+//    @Test
+//    public void shouldReturnCorrectDivision() {
+//        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
+//        Calculator bean = context.getBean(Calculator.class);
+//        double value = bean.divide(10, 2);
+//        Assertions.assertNotNull(value);
+//
+//    }
+}
+
