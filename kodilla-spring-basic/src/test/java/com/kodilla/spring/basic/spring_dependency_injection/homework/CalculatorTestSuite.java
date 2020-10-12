dc3a22f1ae86d63dc170467acdad816902109d39
Agnieszka Kowalkowska <agnieszkakowalkowska@gmail.com>
2020-10-12 14:19:47 +0200

    task-9.2

--- kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_dependency_injection/homework/CalculatorTestSuite.java
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_dependency_injection/homework/CalculatorTestSuite.java
@@ -1,41 +1,48 @@
 package com.kodilla.spring.basic.spring_dependency_injection.homework;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootTest
-public class CalculatorTestSuite {
+class CalculatorTestSuite {
 
-//    @Test
-//    public void shouldReturnCorrectSum() {
-//        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
-//        Calculator bean = context.getBean(Calculator.class);
-//        double value = bean.add(10, 2);
-//        Assertions.assertNotNull(value);
-//        }
-//
-//    @Test
-//    public void shouldReturnCorrectSubtraction() {
-//        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
-//        Calculator bean = context.getBean(Calculator.class);
-//        double value = bean.subtract(10, 2);
-//        Assertions.assertNotNull(value);
-//    }
-//
-//    @Test
-//    public void shouldReturnCorrectMultiplication() {
-//        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
-//        Calculator bean = context.getBean(Calculator.class);
-//        double value = bean.multiply(10, 2);
-//        Assertions.assertNotNull(value);
-//    }
-//
-//    @Test
-//    public void shouldReturnCorrectDivision() {
-//        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
-//        Calculator bean = context.getBean(Calculator.class);
-//        double value = bean.divide(10, 2);
-//        Assertions.assertNotNull(value);
-//
-//    }
+    ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic.spring_dependency_injection");
+    Calculator bean = context.getBean(Calculator.class);
+
+
+    @Test
+    public void shouldReturnCorrectAdditionResultAndDisplayResult(){
+        double result = bean.add(5.0,7.0);
+        Assertions.assertEquals(12.0, result);
+    }
+
+    @Test
+    public void shouldReturnCorrectSubtractionResultAndDisplayResult(){
+        double result = bean.subtract(10.0, 4.0);
+        Assertions.assertEquals(6.0, result);
+    }
+
+    @Test
+    public void shouldReturnCorrectMultiplicationResultAndDisplayResult(){
+        double result = bean.multiply(2.0, 3.5);
+        Assertions.assertEquals(7.0, result);
+    }
+
+    @Test
+    public void shouldReturnCorrectDivisionResultAndDisplayResult() throws ZeroUsageException {
+        double result = bean.divide(11.0, 2.0);
+        Assertions.assertEquals(5.5, result);
+    }
+
+    @Test
+    public void shouldReturnExceptionWhenDividingByZero()  {
+        Assertions.assertThrows(ZeroUsageException.class, ()->{
+            double result = bean.divide(11.0, 0);
+        });
+    }
 }
+
 
