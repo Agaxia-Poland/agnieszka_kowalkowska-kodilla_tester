a50b4dd15d49d37ac77a76663c140d63ff67483b
Agaxia-Poland <66327545+Agaxia-Poland@users.noreply.github.com>
2020-10-17 12:25:44 +0200

    task-10.3.REST-API (#29)
    * task-10.3.REST-API
    
    * task-10.3.REST-API
    
    * task-10.3.REST-API
    
    * task-10.3.REST-API
    
    * task-10.3.REST-API

---
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerTest.java
@@ -0,0 +1,50 @@
+package com.kodilla.rest.controller;
+
+import com.kodilla.rest.domain.BookDto;
+import com.kodilla.rest.service.BookService;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class BookControllerTest {
+
+@Test
+    public void shouldFetchBooks () {
+    //given
+    BookService bookServiceMock = Mockito.mock(BookService.class);
+    BookController bookController = new BookController(bookServiceMock);
+    List<BookDto> booksList = new ArrayList<>();
+    booksList.add(new BookDto("Title 1", "Author 1"));
+    booksList.add(new BookDto("Title 2", "Author 2"));
+    Mockito.when(bookServiceMock.getBooks()).thenReturn(booksList);
+
+    //when
+    List<BookDto> result = bookController.getBooks();
+
+    //then
+    assertThat(result).hasSize(2);
+    }
+
+@Test
+public void shouldAddBook(BookDto bookDto) {
+    //given
+
+    BookService bookServiceMock = Mockito.mock(BookService.class);
+    BookController bookController = new BookController(bookServiceMock);
+    List<BookDto> booksList = new ArrayList<BookDto>();
+    booksList.add(new BookDto("Title 3", "Author 3"));
+    booksList.add(new BookDto("Title 4", "Author 4"));
+    Mockito.when(bookServiceMock.getBooks()).thenReturn(booksList);
+
+    //when
+
+    List<BookDto> result = bookController.getBooks();
+
+    //then
+    assertThat(result).hasSize(2);
+    }
+}
